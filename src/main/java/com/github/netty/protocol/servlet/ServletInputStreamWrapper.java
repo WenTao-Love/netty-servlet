ea3b7ffe20fbec1ea460932334e682570133db31
wangzihao <wangzihao@cc.cc>
2024-06-06 17:50:41 +0800

    同步最新代码, 从 https://github.com/wangzihaogithub/spring-boot-protocol.git 当前项目是servlet项目, spring-boot-protocol项目是tcp协议项目,包含servlet

--- src/main/java/com/github/netty/protocol/servlet/ServletInputStreamWrapper.java
+++ src/main/java/com/github/netty/protocol/servlet/ServletInputStreamWrapper.java
@@ -175,6 +175,7 @@ public class ServletInputStreamWrapper extends javax.servlet.ServletInputStream 
                                     writeSize += w;
                                 }
                             }
+                            source.removeComponents(0, source.numComponents());
                         }
                     }
                     int w = outputChannel.write(byteBuf.nioBuffer());
